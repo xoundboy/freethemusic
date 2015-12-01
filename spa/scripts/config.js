b9fc9b47817731c63df7db9a2ded79d4a143c285
Ben Roberts <xoundboy@gmail.com>
2015-12-01 13:52:37 +0100

    missed adding the config file before

---
+++ spa/scripts/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SERVER_PORT:        process.env.X71_SERVER_PORT,
+    AUDIO_LIBRARY_PATH: process.env.X71_LIB_PATH + "audio/",
+    IMAGE_LIBRARY_PATH: process.env.X71_LIB_PATH + "images/",
+    DB_HOST:            process.env.X71_DB_HOST,
+    DB_NAME:            process.env.X71_DB_NAME,
+    DB_USER:            process.env.X71_DB_USER,
+    DB_PASS:            process.env.X71_PB_PASS
+};
